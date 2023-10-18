28c3cf601bf6dcb4542aa6032ecb1ed4eda22e19
hs1lva <120216693+hs1lva@users.noreply.github.com>
2023-10-18 19:45:07 +0100

    teste

--- prog-orientada-obj/projecto-de-gestao-de-transacoes-imobiliarias/Class/Person.cs
+++ prog-orientada-obj/projecto-de-gestao-de-transacoes-imobiliarias/Class/Person.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Projeto_de_Gestao_de_Transacoes_Imobiliarias.Classes
+namespace Projeto_de_Gestao_de_Transacoes_Imobiliarias.Class
 {
     public class Person
     {
