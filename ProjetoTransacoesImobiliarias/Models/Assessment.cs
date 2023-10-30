71089c239f716a518dda90ca6a95c00963fd463e
PRS10 <87570813+PRS10@users.noreply.github.com>
2023-10-30 10:37:10 +0000

    ajustments on Proposal and Assessment class

--- ProjetoTransacoesImobiliarias/Models/Assessment.cs
+++ ProjetoTransacoesImobiliarias/Models/Assessment.cs
@@ -7,6 +7,8 @@ public class Assessment
     protected int IdEvaluator { get; set; }
     protected int IdProperty { get; set; }
     protected DateAndTime? AssessmentDate { get; set; }
-
     protected decimal AssessmentValue { get; set; }
+    protected bool Active;
+
+    protected static List<Assessment> AssessmentList = new List<Assessment>();
 }
--- ProjetoTransacoesImobiliarias/Models/Proposal.cs
+++ ProjetoTransacoesImobiliarias/Models/Proposal.cs
@@ -10,6 +10,6 @@ public class Proposal
     protected decimal ProposalValue { get; set; }
     protected DateAndTime? ProposalDate {get ; set ;}
     protected DateAndTime? ProposalDateLimit {get ; set ;}
-    
     protected bool Active;
+    protected static List<Proposal> ProposalList = new List<Proposal>();
 }
