1619f1344a6c059cc696770b42010710278819dd
Pedro Ricardo <pedroricardo10@gmail.com>
2023-12-09 19:07:57 +0000

    ProposalController and ProposarView created

--- ProjetoTransacoesImobiliarias/Models/Proposal.cs
+++ ProjetoTransacoesImobiliarias/Models/Proposal.cs
@@ -5,12 +5,31 @@ namespace ProjetoTransacoesImobiliarias;
 
 public class Proposal
 {
-    private int ProposalId { get; set; }
-    protected int ClientId { get; set; }
-    protected int PropertyId { get; set; }
+    private static int ProposalIdCounter = 0;
+    public int ProposalId { get; set; }
+    public int ClientId { get; set; }
+    public int PropertyId { get; set; }
     protected decimal ProposalValue { get; set; }
-    protected DateAndTime? ProposalDate {get ; set ;}
-    protected DateAndTime? ProposalDateLimit {get ; set ;}
-    protected bool Active;
-    protected static List<Proposal> ProposalList = new List<Proposal>();
+    public DateTime? ProposalDate {get ; set ;}
+    public DateTime? ProposalAceptedDate {get ; set ;}
+    public DateTime? ProposalDateLimit {get ; set ;}
+    public bool Active;
+    public static List<Proposal> ProposalList = new List<Proposal>();
+
+    public Proposal( int clientId, int propertyId, decimal proposalValue)
+    {
+        ProposalId = ProposalIdCounter++;
+        ClientId = clientId;
+        PropertyId = propertyId;
+        ProposalValue = proposalValue;
+        //ProposalDate = proposalDate;
+        //ProposalDateLimit = proposalDateLimit;
+        Active = true;
+        ProposalList.Add(this);
+    }
+    public Proposal()
+    {
+        //Empty necessario?
+    }
+
 }
---
+++ ProjetoTransacoesImobiliarias/Views/CLI/Proposal/ProposalView.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoTransacoesImobiliarias.Views.CLI.Proposal
+{
+    public class ProposalView
+    {
+        public string? ShowProposalMenu()
+        {
+            // vale a pensa utilizar isto, não é complicar?
+            Menu.ProposalMenu();
+            return Console.ReadLine();
+        }
+
+        public int ProposalViewIdProperty()
+        {
+            int id;
+            Console.Clear();
+            Console.Write("Id da propriedade");
+            id = Convert.ToInt32(Console.ReadLine());
+            return id;
+        }
+
+        public int ProposalViewIdClient()
+        {
+            int id;
+            Console.Clear();
+            Console.Write("Id do cliente");
+            id = Convert.ToInt32(Console.ReadLine());
+            return id;
+        }
+
+        public decimal ProposalViewProposalValue()
+        {
+            decimal value;
+            Console.Clear();
+            Console.Write("Valor");
+            value = Convert.ToDecimal(Console.ReadLine());
+            return value;
+        }
+
+        public void Print <T>(List<T> list)
+        {
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+
+    }
+}
